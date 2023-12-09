d41cda8765418ec0face10475c5a92bc93e633e4
hyungjun-tech01 <whmoon00@naver.com>
2023-12-09 14:53:10 +0900

    button

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -3,10 +3,25 @@ import {Stack, Button} from "@mui/material";
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>               
+    <Stack spacing={4} direction="column">
+      <Stack spacing={2} direction="row">
+          <Button variant="text" href="https://google.com">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>               
+      </Stack>
+      <Stack spacing={2} direction="row">
+          <Button variant="contained" color="primary">Primary</Button>
+          <Button variant="contained" color="secondary">Secondary</Button>
+          <Button variant="contained" color="error">Error</Button>
+          <Button variant="contained" color="warning">Warning</Button>
+          <Button variant="contained" color="info">Info</Button>
+          <Button variant="contained" color="success">Success</Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+          <Button variant="contained" color="primary" size="small">Small</Button>
+          <Button variant="contained" color="primary" size="medium">Medium</Button>
+          <Button variant="contained" color="primary" size="large">large</Button>
+      </Stack>      
     </Stack>
   )
 }
